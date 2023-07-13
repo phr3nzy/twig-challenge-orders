6ea4ec9eef8d4b5f66b1593691b562d2d3cb45d9
phr3nzy <adilosama47@gmail.com>
2023-07-13 11:12:53 +0400

    restructure event handling and handle more events

--- lib/server/services/events.ts
+++ lib/server/services/events.ts
@@ -1,33 +1,86 @@
 import type { FastifyInstance } from 'fastify';
 
+type OrdersQueuePayload =
+	| {
+			message: 'payment-completed';
+			data: {
+				paymentId: string;
+				status: 'created' | 'completed';
+			};
+	  }
+	| {
+			message: 'orders-email-sent';
+			data: {
+				orderId: string;
+			};
+	  }
+	| {
+			message: 'payments-email-sent';
+			data: {
+				email: string;
+				paymentId: string;
+			};
+	  };
+
 export default async function events(app: FastifyInstance) {
 	await app.queue.channel.consume(app.config.ORDERS_QUEUE_NAME, async msg => {
 		if (msg) {
-			const { message, data } = JSON.parse(msg.content.toString()) as {
-				message: string;
-				data: {
-					paymentId: string;
-					status: 'created' | 'completed';
-				};
-			};
+			const { message, data } = JSON.parse(
+				msg.content.toString(),
+			) as OrdersQueuePayload;
 
-			const { paymentId, status } = data;
+			switch (message) {
+				case 'payment-completed': {
+					const { paymentId, status } = data;
 
-			const order = await app.db.order.findUnique({
-				where: { paymentId },
-			});
+					const order = await app.db.order.findUnique({
+						where: { paymentId },
+					});
 
-			if (order && status === 'completed' && message === 'payment-completed') {
-				await app.db.order.update({
-					where: { id: order.id },
-					data: {
-						status: 'completed',
-					},
-				});
+					if (order && status === 'completed') {
+						await app.db.order.update({
+							where: { id: order.id },
+							data: {
+								status: 'completed',
+							},
+						});
+					}
+					break;
+				}
+				case 'orders-email-sent': {
+					const { orderId } = data;
 
-				app.log.info(`Order ${order.id} completed.`);
-			} else {
-				app.log.error('Order not found.');
+					const order = await app.db.order.findUnique({
+						where: { id: orderId },
+					});
+
+					if (order && order.status === 'completed') {
+						await app.db.order.update({
+							where: { id: orderId },
+							data: {
+								orderEmailSent: true,
+							},
+						});
+					}
+					break;
+				}
+				case 'payments-email-sent': {
+					const { paymentId } = data;
+
+					const order = await app.db.order.findUnique({
+						where: { paymentId },
+					});
+
+					if (order && order.status === 'completed') {
+						await app.db.order.update({
+							where: { id: order.id },
+							data: {
+								paymentEmailSent: true,
+							},
+						});
+					}
+					break;
+				}
 			}
 
 			app.queue.channel.ack(msg);
